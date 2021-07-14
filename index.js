4f6959ccd49f30114e0dccfb169b6f9cc0f19eaa
Gevorg Martirosyan <gevorg.martirosyan.1998.08@gmail.com>
2021-07-14 23:25:54 +0400

    Correct homework

--- index.js
+++ index.js
@@ -1,53 +1,26 @@
+// homework
+
+const argv = require('minimist')(process.argv.slice(2));
 const events = require('events');
 
-const logger = new events()
+const getMessages = new events();
+const getUsers = new events();
 
+const messages = [];
 const users = [];
-const msgs = [];
 
-logger.on('message', (msg) => {
-    console.log(`New message: ${msg}`);
-    msgs.push(msg)
-})
+getMessages.on("addMessages", addData);
+getMessages.emit("addMessages", messages, argv.message);
+console.log(messages)
 
-logger.emit("message", "hello world");
+getUsers.on("addUsers", addData);
+getUsers.emit("addUsers", users, argv.addUser);
+console.log(users);
 
-logger.on('login', name => {
-    console.log(`new user: ${name}`)
-    users.push(name)
-})
-
-logger.emit('message', 'there is new message');
-logger.emit('login', 'name1');
-logger.emit('login', 'name2');
-
-
-logger.on('getUsers', () => {
-    console.log('logged users:')
-    users.forEach(name => {
-        console.log(name)
-    })
-
-})
-
-logger.on('getMessage', () => {
-    console.log('messages');
-    msgs.forEach(msg => {
-        console.log(msg)
-    })
-})
-
-logger.emit('getUsers')
-logger.emit('getMessage')
-
-
-// homework
-console.log("----- HI, THERE IS MY HOMEWORK -----")
-
-const minimist = require('minimist')(process.argv.slice(2));
-
-users.push(minimist.addUser);
-msgs.push(minimist.message);
-
-console.log(`Messages: ${msgs}`);
-console.log(`Users: ${users}`);
+function addData(arr, property) {
+    if (property) {
+        arr.push(property)
+    } else {
+        arr.push('There is no any data')
+    }
+}
